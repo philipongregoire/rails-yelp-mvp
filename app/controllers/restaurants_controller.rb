b85ed64e0771647ae4b866870b8baf4195b56427
Gregoire Philipon <philipon.gregoire@gmail.com>
2017-05-17 17:52:39 +0200

    MVC rails Restaurant done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,35 @@
+class RestaurantsController < ApplicationController
+
+before_action :find_restaurant, only: [:show, :edit]
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @reviews = @restaurant.reviews
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+estaminet = Restaurant.create(
+  name: "Milad",
+  address: "75002 Paris",
+  phone_number: "003322",
+  category: "japanese"
+  )
+bristol = Restaurant.create(
+  name: "Epicure",
+  address: "75011 Paris",
+  phone_number: "002233",
+  category: "french"
+  )
+brasseurs = Restaurant.create(
+  name: "Cesar",
+  address: "75009 Paris",
+  phone_number: "004433",
+  category: "belgian"
+  )
+fourchette = Restaurant.create(
+  name: "Alfonse",
+  address: "75003 Paris",
+  phone_number: "006622",
+  category: "japanese"
+  )
+couteau = Restaurant.create(
+  name: "José",
+  address: "75006 Paris",
+  phone_number: "005511",
+  category: "chinese"
+  )
